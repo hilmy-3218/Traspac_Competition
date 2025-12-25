06b2754bcdcaaec72dfb8febe11314443fbe9ed6
hilmy-3218 <hilmyter321@gmail.com>
2025-12-25 10:27:23 +0700

    Update rank.php

--- elearning/public/rank.php
+++ elearning/public/rank.php
@@ -121,7 +121,7 @@ try {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <title>Ranking - <?= htmlspecialchars($nama); ?></title>
+    <title>Ranking - <?= htmlspecialchars($nama, ENT_QUOTES, 'UTF-8'); ?></title>
     <link rel="shortcut icon" href="../assets/images/logo.png" type="image/x-icon">
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700;800&display=swap" rel="stylesheet">
@@ -366,3 +366,4 @@ try {
     <?php require_once __DIR__ . '/../private/nav-bottom.php';?>
 </body>
 </html>
+
