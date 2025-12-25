1193e4b64b38780ec0a9012dda1bd6f0ca417a71
hilmy-3218 <hilmyter321@gmail.com>
2025-12-25 09:41:59 +0700

    Update dashboard.php

--- elearning/public/dashboard.php
+++ elearning/public/dashboard.php
@@ -298,7 +298,7 @@ if ($role === 'siswa') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard - <?php echo $role; ?></title>
+    <title>Dashboard - <?= htmlspecialchars($role, ENT_QUOTES, 'UTF-8'); ?></title>
     <link rel="shortcut icon" href="../assets/images/logo.png" type="image/x-icon">
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700;800&display=swap" rel="stylesheet">
@@ -927,4 +927,5 @@ if ($role === 'siswa') {
 
 </script>
 </body>
+
 </html>
